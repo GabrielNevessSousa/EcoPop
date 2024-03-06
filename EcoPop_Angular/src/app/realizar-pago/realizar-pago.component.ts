ea3ed14f49c0d3507f52000d84d1e70bd0bf0fde
Kenny <kennyacordido@gmail.com>
2024-03-06 17:11:26 +0100

    updata pago

--- EcoPop_Angular/src/app/realizar-pago/realizar-pago.component.ts
+++ EcoPop_Angular/src/app/realizar-pago/realizar-pago.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-realizar-pago',
   templateUrl: './realizar-pago.component.html',
   styleUrls: ['./realizar-pago.component.css']
 })
-export class RealizarPagoComponent implements OnInit {
+export class RealizarPagoComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  pagoForm: FormGroup= new FormGroup({
+    titular: new FormControl('',[Validators.required]),
+    numero: new FormControl(null,[Validators.required]),
+    fecha: new FormControl(null,[Validators.required]),
+    cvv: new FormControl(null,[Validators.required]),
+    
+  })
 
 }
